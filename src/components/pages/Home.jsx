65ce7a5afdb0405e165b160a04575dd93b2e79e2
TomyAlberdi <tomas.alberdi.2001@gmail.com>
2023-03-11 19:03:27 -0300

    Reestructuración 3

--- src/components/pages/Home.jsx
+++ src/components/pages/Home.jsx
@@ -3,7 +3,15 @@ import React from 'react'
 const Home = () => {
     return (
         <div className='Main Home'>
-            <h1>Home</h1>
+            <div>
+                <h2>ey</h2>
+            </div>
+            <div>
+                <h2>eyy</h2>
+            </div>
+            <div>
+                <h2>eyyy</h2>
+            </div>
         </div>
     )
 }
