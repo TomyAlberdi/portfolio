081eb65b786c5c478d38bfbbacc08a7e5900156f
TomyAlberdi <tomas.alberdi.2001@gmail.com>
2023-03-11 17:53:38 -0300

    Reestructuración 1

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,6 @@
 import './sass/App.css'
 import { Routes, Route } from 'react-router-dom'
 import Navbar from './components/Navbar'
-import Footer from './components/Footer'
 import Home from './components/pages/Home'
 import Projects from './components/pages/Projects'
 import Contact from './components/pages/Contact'
@@ -17,7 +16,6 @@ function App() {
                     <Route path='/contact' element={<Contact />} />
                 </Routes>
             </main>
-            <Footer />
         </div>
     )
 }
