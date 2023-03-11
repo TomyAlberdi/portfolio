8925db8825aa8bbd15084d17ea29100011336280
TomyAlberdi <tomas.alberdi.2001@gmail.com>
2023-03-11 17:23:04 -0300

    Deploy with custom domain v1

--- src/App.jsx
+++ src/App.jsx
@@ -12,9 +12,9 @@ function App() {
             <Navbar />
             <main>
                 <Routes>
-                    <Route exact path='/Portfolio/' element={<Home />} />
-                    <Route path='/Portfolio/projects' element={<Projects />} />
-                    <Route path='/Portfolio/contact' element={<Contact />} />
+                    <Route exact path='/' element={<Home />} />
+                    <Route path='/projects' element={<Projects />} />
+                    <Route path='/contact' element={<Contact />} />
                 </Routes>
             </main>
             <Footer />
