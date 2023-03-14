c8b835216be184e9472e596bc64bcc1e5342e2f3
TomyAlberdi <tomas.alberdi.2001@gmail.com>
2023-03-13 23:22:06 -0300

    Complete style change (test)

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,29 @@
 import './sass/App.css'
-import { Routes, Route } from 'react-router-dom'
-import Navbar from './components/Navbar'
+import { Routes, Route, Link, useLocation } from 'react-router-dom'
 import Home from './components/pages/Home'
 import Projects from './components/pages/Projects'
 import Contact from './components/pages/Contact'
 
 function App() {
+
+    let getLocation = () => {
+        return useLocation().pathname
+    }
+
     return (
         <div className="App">
-            <Navbar />
             <main>
+                <header>
+                    <div className="pres">
+                        <h1>Tomás Alberdi</h1>
+                        <h3>Web Developer</h3>
+                    </div>
+                    <nav>
+                        <Link to={"/"}>{getLocation() === "/" ? <i className="fa-solid fa-circle"></i> : ""}Home</Link>
+                        <Link to={"/projects"}>{getLocation() === "/projects" ? <i className="fa-solid fa-circle"></i> : ""}Projects</Link>
+                        <Link to={"/contact"}>{getLocation() === "/contact" ? <i className="fa-solid fa-circle"></i> : ""}Contact</Link>
+                    </nav>
+                </header>
                 <Routes>
                     <Route exact path='/' element={<Home />} />
                     <Route path='/projects' element={<Projects />} />
--- src/components/pages/Home.jsx
+++ src/components/pages/Home.jsx
@@ -1,20 +1,9 @@
 import React from 'react'
-import Card from '../utils/Card'
 
 const Home = () => {
     return (
         <div className='Main Home'>
-            <section className="pres">
-                <h1>Tomás Alberdi</h1>
-                <h3>Web Developer</h3>
-            </section>
-            <section className="content">
-                <Card />
-                <Card />
-                <Card />
-                <Card />
-                <Card />
-            </section>
+
         </div>
     )
 }
