f5cdfd18052398f091ad1c65f76775126b6b4207
TomyAlberdi <tomas.alberdi.2001@gmail.com>
2023-03-25 18:38:14 -0300

    Advanced Projects

--- src/components/pages/Projects.jsx
+++ src/components/pages/Projects.jsx
@@ -5,44 +5,76 @@ const Projects = () => {
         <div className='Main Projects'>
             <ul>
                 <li>
-                    <span className="date">año</span>
-                    <span className="nombre">nombre Proyecto</span>
-                    <div>tec</div>
+                    <span className="date">2022</span>
+                    <span className="nombre">Nombre Proyecto</span>
+                    <div class="icons">
+                        <i class="fa-brands fa-html5"></i>
+                        <i class="fa-brands fa-sass"></i>
+                        <i class="fa-brands fa-js"></i>
+                    </div>
                 </li>
                 <li>
-                    <span className="date">año</span>
-                    <span className="nombre">nombre Proyecto</span>
-                    <div>tec</div>
+                    <span className="date">2022</span>
+                    <span className="nombre">Nombre Proyecto</span>
+                    <div class="icons">
+                        <i class="fa-brands fa-html5"></i>
+                        <i class="fa-brands fa-sass"></i>
+                        <i class="fa-brands fa-js"></i>
+                    </div>
                 </li>
                 <li>
-                    <span className="date">año</span>
-                    <span className="nombre">nombre Proyecto</span>
-                    <div>tec</div>
+                    <span className="date">2022</span>
+                    <span className="nombre">Nombre Proyecto</span>
+                    <div class="icons">
+                        <i class="fa-brands fa-html5"></i>
+                        <i class="fa-brands fa-sass"></i>
+                        <i class="fa-brands fa-js"></i>
+                    </div>
                 </li>
                 <li>
-                    <span className="date">año</span>
-                    <span className="nombre">nombre Proyecto</span>
-                    <div>tec</div>
+                    <span className="date">2022</span>
+                    <span className="nombre">Nombre Proyecto</span>
+                    <div class="icons">
+                        <i class="fa-brands fa-html5"></i>
+                        <i class="fa-brands fa-sass"></i>
+                        <i class="fa-brands fa-js"></i>
+                    </div>
                 </li>
                 <li>
-                    <span className="date">año</span>
-                    <span className="nombre">nombre Proyecto</span>
-                    <div>tec</div>
+                    <span className="date">2022</span>
+                    <span className="nombre">Nombre Proyecto</span>
+                    <div class="icons">
+                        <i class="fa-brands fa-html5"></i>
+                        <i class="fa-brands fa-sass"></i>
+                        <i class="fa-brands fa-js"></i>
+                    </div>
                 </li>
                 <li>
-                    <span className="date">año</span>
-                    <span className="nombre">nombre Proyecto</span>
-                    <div>tec</div>
+                    <span className="date">2022</span>
+                    <span className="nombre">Nombre Proyecto</span>
+                    <div class="icons">
+                        <i class="fa-brands fa-html5"></i>
+                        <i class="fa-brands fa-sass"></i>
+                        <i class="fa-brands fa-js"></i>
+                    </div>
                 </li>
                 <li>
-                    <span className="date">año</span>
-                    <span className="nombre">nombre Proyecto</span>
-                    <div>tec</div>
+                    <span className="date">2022</span>
+                    <span className="nombre">Nombre Proyecto</span>
+                    <div class="icons">
+                        <i class="fa-brands fa-html5"></i>
+                        <i class="fa-brands fa-sass"></i>
+                        <i class="fa-brands fa-js"></i>
+                    </div>
                 </li>
                 <li>
-                    <span className="date">año</span>
-                    <span className="nombre">nombre Proyecto</span>
-                    <div>tec</div>
+                    <span className="date">2022</span>
+                    <span className="nombre">Nombre Proyecto</span>
+                    <div class="icons">
+                        <i class="fa-brands fa-html5"></i>
+                        <i class="fa-brands fa-sass"></i>
+                        <i class="fa-brands fa-js"></i>
+                    </div>
                 </li>
             </ul>
         </div>
