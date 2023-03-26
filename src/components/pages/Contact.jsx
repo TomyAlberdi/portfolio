d7217b1d9de98cac27d4bc4036f9e8f39dd2e7cb
TomyAlberdi <tomas.alberdi.2001@gmail.com>
2023-03-26 19:57:32 -0300

    Finish Contact Page (Lacks contact form)

--- src/components/pages/Contact.jsx
+++ src/components/pages/Contact.jsx
@@ -1,12 +1,13 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 const Contact = () => {
 
     const copyMail = () => {
         navigator.clipboard.writeText("tomas.alberdi.2001@gmail.com")
-        let sec = document.querySelector(".copyMail section")
-        let icon = document.querySelector(".copyMail section i")
-        let text = document.querySelector(".copyMail section h2")
+        let sec = document.querySelector(".copyMail a")
+        let icon = document.querySelector(".copyMail a i")
+        let text = document.querySelector(".copyMail a h2")
         sec.classList.add("copied")
         icon.classList.remove("fa-copy")
         icon.classList.remove("fa-regular")
@@ -18,22 +19,22 @@ const Contact = () => {
     return (
         <div className='Main Contact'>
             <div className='link div1'>
-                <section>
+                <Link to="https://github.com/TomyAlberdi" target="_blank">
                     <i className="fa-brands fa-github"></i>
                     <span>
                         <h3>GitHub</h3>
                         <i className="fa-solid fa-link"></i>
                     </span>
-                </section>
+                </Link>
             </div>
             <div className='link div2'>
-                <section>
+                <Link to="https://www.linkedin.com/in/tom%C3%A1s-alberdi-42150a219/" target="_blank">
                     <i className="fa-brands fa-linkedin"></i>
                     <span>
                         <h3>LinkedIn</h3>
                         <i className="fa-solid fa-link"></i>
                     </span>
-                </section>
+                </Link>
             </div>
             <div className='copyMail div3'>
                 <section onClick={copyMail}>
