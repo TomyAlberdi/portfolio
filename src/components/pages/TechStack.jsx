30e34d999b9cf1694f8c0daf0b298d730bb79801
Tomás Alberdi <tomas.alberdi.2001@gmail.com>
2023-09-30 16:46:40 -0300

    Added Tech Stack page basic structure desktop

--- src/components/pages/TechStack.jsx
+++ src/components/pages/TechStack.jsx
@@ -3,7 +3,20 @@ import React from 'react'
 const TechStack = ({Español}) => {
   return (
     <div className='Main TechStack'>
-      TechStack
+      <ul>
+        <li className="tech frontend">
+          <h3>Frontend</h3>
+        </li>
+        <li className="tech backend">
+          <h3>Backend</h3>
+        </li>
+        <li className="tech database">
+          <h3>Database</h3>
+        </li>
+        <li className="tech infrastructure">
+          <h3>Infrastructure</h3>
+        </li>
+      </ul>
     </div>
   )
 }
