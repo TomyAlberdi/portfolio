274f60b49267e44f9369a6d965b2e99aae9e6260
Tomás Alberdi <tomas.alberdi.2001@gmail.com>
2023-09-30 18:02:35 -0300

    Added frontend tech stack icons

--- src/components/pages/TechStack.jsx
+++ src/components/pages/TechStack.jsx
@@ -6,6 +6,28 @@ const TechStack = ({Español}) => {
       <ul>
         <li className="tech frontend">
           <h3>{ Español ? "Desarrollo Frontend" : "Frontend Development"}</h3>
+          <section className="tech_details">
+            <div>
+              <i className="fa-brands fa-html5"></i>
+              HTML
+            </div>
+            <div>
+              <i className="fa-brands fa-css3-alt"></i>
+              CSS
+            </div>
+            <div>
+              <i className="fa-brands fa-sass"></i>
+              SASS
+            </div>
+            <div>
+              <i className="fa-brands fa-react"></i>
+              React
+            </div>
+            <div>
+            <i className="fa-brands fa-js"></i>
+              JavaScript
+            </div>
+          </section>
         </li>
         <li className="tech backend">
           <h3>{ Español ? "Desarrollo Backend" : "Backend Development"}</h3>
